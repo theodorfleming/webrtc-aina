82f8d0681808e2bda3da4d1a7600fc762cd971a1
theo <thefl596@student.liu>
2016-05-05 11:11:29 +0200

    first commit

---
+++ app/src/main/java/com/example/theo/communication/Constants.java
@@ -0,0 +1,15 @@
+package com.example.theo.communication;
+
+/**
+ * TODO: Change "demo" to your Pub and Sub Keys
+ * TODO: Create a standby suffix to differentiate SDP messages from stand by messages
+ */
+public class Constants {
+    public static final String SHARED_PREFS = "SHARED_PREFS";
+    public static final String USER_NAME    = "USERNAME";
+    public static final String PUB_KEY = "pub-c-8b207361-0eb3-4e72-97df-8377c62df520"; // Your Pub Key
+    public static final String SUB_KEY = "sub-c-6bd0d468-0b84-11e6-bb6c-02ee2ddab7fe"; // Your Sub Key
+    public static final String STDBY_SUFFIX = "-stdby";
+    public static final String JSON_CALL_USER = "call_user";
+    public static final String CALL_USER = "call_user";
+}
